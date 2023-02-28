75194e4a52f9e50309c3d8a7b88bc1d2e6f78e9c
BojanOD <bojanancev@gmail.com>
2023-02-28 11:03:14 +0100

    Nextcloud unlink

--- models/ir_attachments.py
+++ models/ir_attachments.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+
+import requests
 
 from odoo import fields, models
+from odoo.http import request
 
 
 class IrAttachments(models.Model):
@@ -10,6 +14,24 @@ class IrAttachments(models.Model):
     nextcloud_attachment = fields.Boolean(string="Nextcloud Attachment", readonly="True")
     nextcloud_view_link = fields.Char(string="Nextcloud View Link")
     nextcloud_share_link = fields.Char(string="Nextcloud Share Link")
+
+    def unlink(self):
+        nextcloud_attachments = self.filtered(lambda l: l.nextcloud_attachment)
+        if nextcloud_attachments:
+            url = request.env['ir.config_parameter'].sudo().get_param('nextcloud.nextcloud_url')
+            username = request.env['ir.config_parameter'].sudo().get_param('nextcloud.nextcloud_username')
+            password = request.env['ir.config_parameter'].sudo().get_param('nextcloud.nextcloud_password')
+            head = {'OCS-APIRequest': 'true'}
+            for record in nextcloud_attachments:
+                filename = record.name
+                url = url + '/remote.php/dav/files/' + username + '/' + filename
+                delete_file = requests.delete(
+                    url,
+                    headers=head,
+                    auth=(username, password)
+                )
+                logging.info(delete_file)
+        return super().unlink()
 
 
 class Document(models.Model):
