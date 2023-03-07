0d5f2bd426596d46926eae301db4cbf402017c1b
Sinisha <m97@simplify-erp.de>
2023-03-07 09:14:34 +0000

    NextCloud

--- models/ir_attachments.py
+++ models/ir_attachments.py
@@ -34,9 +34,3 @@ class IrAttachments(models.Model):
         return super().unlink()
 
 
-class Document(models.Model):
-    _inherit = 'documents.document'
-
-    nextcloud_attachment = fields.Boolean(string="Nextcloud Attachment", related='attachment_id.nextcloud_attachment')
-    nextcloud_view_link = fields.Char(string="Nextcloud View Link", related='attachment_id.nextcloud_view_link')
-    nextcloud_share_link = fields.Char(string="Nextcloud Share Link", related='attachment_id.nextcloud_share_link')
